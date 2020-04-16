3d4663e1d645041134d4a83103980169d354a2f5
jrpotter <jrpotter2112@gmail.com>
2020-04-15 22:48:12 -0400

    Add a more usable shell this time around.

---
+++ shell.nix
@@ -0,0 +1,23 @@
+{ miso ? import ./miso, compiler ? "ghc865" }:
+let
+  def = (import ./default.nix {});
+  # Reference to a version of ghcide that matches our compiler. Note the
+  # latest version will have issues with mismatched GLIBC versions.
+  rev = "0ae8d9869ace81c4efaa279379c5a716280cb2b7";
+  link = "https://github.com/cachix/ghcide-nix/tarball/${rev}";
+  ghcide = (import (builtins.fetchTarball link) {})."ghcide-${compiler}";
+  # Wrapper to inject our development fields into our nested environments.
+  wrapper = env: env.overrideAttrs (oldAttrs: {
+    buildInputs = oldAttrs.buildInputs ++ [ ghcide ];
+    # Hoogle by default will try to generate a database where it cannot.
+    # Additionally, certain extensions of Visual Studio Code will assume the
+    # hoogle database exists where it doesnt since this is running from nix.
+    # Instead specify to a locally generated hoogle database created by running
+    # `hoogle generate --database=hoogle/portfolio.hoo`.
+    HIE_HOOGLE_DATABASE = ./hoogle/portfolio.hoo;
+  });
+in
+  {
+    backend = wrapper def.backend.env;
+    frontend = wrapper def.frontend.env;
+  }
