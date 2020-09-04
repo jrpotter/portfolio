f914f2800dd7f003067651951a152a09c63b1ca5
Joshua Potter <jrpotter2112@gmail.com>
2020-09-04 10:34:02 -0400

    Include the HtmlWebpackPlugin.

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,11 @@
-const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const common = require('./webpack.common.js');
+const { merge } = require('webpack-merge');
 
 module.exports = merge(common, {
   mode: 'development',
+  plugins: [
+    // Autogenerates an index.html file for our webpack dev server to load.
+    new HtmlWebpackPlugin(),
+  ],
 });
