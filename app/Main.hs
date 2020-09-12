f4220322961b3ac731f30496d47f3f860ac3a32d
Joshua Potter <jrpotter2112@gmail.com>
2020-09-12 11:48:57 -0400

    Move to static directory.

--- app/Main.hs
+++ app/Main.hs
@@ -6,10 +6,12 @@ import Data.Function (($))
 import Data.Monoid (mconcat)
 import System.IO (IO)
 
+import qualified Network.Wai.Middleware.Static as M
 import qualified Web.Scotty as WS
 
 main :: IO ()
-main = WS.scotty 3000 $
+main = WS.scotty 3000 $ do
+  WS.middleware M.static
   WS.get "/:word" $ do
     beam <- WS.param "word"
     WS.html $ mconcat ["<h1>Scotty, ", beam, "me up!"]
--- web/webpack.config.js
+++ web/webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   },
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, '../static'),
     filename: 'bundle.js',
   },
   module: {
