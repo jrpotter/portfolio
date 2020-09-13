61757380454b375739880ed7236c056676ef20d0
Joshua <jrpotter2112@gmail.com>
2020-09-12 21:12:38 -0400

    Add a sqlite database.

--- src/Main.hs
+++ src/Main.hs
@@ -5,23 +5,32 @@ module Main
 ( main
 ) where
 
+import Control.Monad.Reader (ReaderT)
 import Data.Function (($))
+import Data.Int (Int)
 import Data.Maybe (Maybe(Just))
 import Data.Monoid (mconcat)
 import Data.String (String)
 import Network.Wai.Middleware.Static ((>->))
 import System.IO (FilePath, IO)
 
-import qualified Network.Wai.Middleware.Static as M
-import qualified Web.Scotty as WS
+import qualified Database.SQLite.Simple as Simple
+import qualified Network.Wai.Middleware.Static as Static
+import qualified Web.Scotty as Scotty
 
-policy :: M.Policy
-policy = M.policy rewrite >-> M.addBase "dist"
+data Config = Config { connection :: Simple.Connection }
+
+policy :: Static.Policy
+policy = Static.policy rewrite >-> Static.addBase "dist"
   where
     rewrite :: String -> Maybe String
     rewrite "" = Just "index.html"
     rewrite p = Just p
 
 main :: IO ()
-main = WS.scotty 3000 $ do
-  WS.middleware $ M.staticPolicyWithOptions M.defaultOptions policy
+main = do
+  connection <- Simple.open "db/portfolio.db"
+  let config = Config { connection = connection }
+  Scotty.scotty 3000 $ do
+    let options = Static.defaultOptions
+    Scotty.middleware $ Static.staticPolicyWithOptions options policy
